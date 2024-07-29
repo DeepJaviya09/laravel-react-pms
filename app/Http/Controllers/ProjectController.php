838ebb92a43a932bde50426cb7cdc05cf2276621
Deep Javiya <deepjaviya98@gmail.com>
2024-07-29 22:16:37 +0530

    project edit and update

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -98,7 +98,9 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return inertia('Project/Edit', [
+            'project' => new ProjectResource($project),
+        ]);
     }
 
     /**
@@ -106,7 +108,20 @@ class ProjectController extends Controller
      */
     public function update(UpdateProjectRequest $request, Project $project)
     {
-        //
+        $data = $request->validated();
+        // dd($data);
+        $image = $data['image'] ?? null;
+        $data['updated_by'] = Auth::id();
+        
+        if ($image) {
+            if ($project->image_path) {
+                Storage:disk('public')->delete($project->image_path);
+            }
+            $data['image_path'] = $image->store('project/'.Str::random(), 'public' );
+        }
+        $project->update($request->validated());
+
+        return to_route('project.index')->with('success', "Project {$project->name} hasbeen updated !");
     }
 
     /**
@@ -114,6 +129,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $name = $project->name;
+        $project->delete();
+        return to_route('project.index')->with('success', "Project {$name} deleted!");
     }
 }
