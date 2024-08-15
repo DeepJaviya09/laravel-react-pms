2822876feb86982624ae207ae7c29114783daff0
Deep Javiya <deepjaviya98@gmail.com>
2024-08-15 18:54:24 +0530

    trustProxis

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+
     }
 
     /**
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        if($this->app->evironment('production')){
+            URL::forceScheme('https');
+        }
+        // Schema::defaultStringLength(191);
     }
 }
